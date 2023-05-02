a1f40d63af93def405a8dd7581f842967171a86f
An Tran <tran@slurm-login.ukp.informatik.tu-darmstadt.de>
2023-05-02 22:08:38 +0200

    update sh to run slurm job

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-pushd /ukp-storage-1/tran/DAMO-ConvAI/acl23doc2dial/ &&\
-export HOME=/ukp-storage-1/tran/DAMO-ConvAI/acl23doc2dial/ &&\
-python /ukp-storage-1/tran/DAMO-ConvAI/acl23doc2dial/train_retrieval.py &&\
+pushd /ukp-storage-1/tran/acl23doc2dial/ &&\
+export HOME=/ukp-storage-1/tran//acl23doc2dial/ &&\
+echo "== START TRAINING ==" &&\
+python /ukp-storage-1/tran/acl23doc2dial/train_retrieval.py  @args.txt &&\
 echo "train_retrieval finished..." &&\
-python /ukp-storage-1/tran/DAMO-ConvAI/acl23doc2dial/train_rerank.py &&\
+python /ukp-storage-1/tran/acl23doc2dial/train_rerank.py  @args.txt &&\
 echo "train_rerank finished..." &&\
-python /ukp-storage-1/tran/DAMO-ConvAI/acl23doc2dial/train_generation.py @args.txt &&\
+python /ukp-storage-1/tran/acl23doc2dial/train_generation.py @args.txt &&\
 echo "train_generation finished..." &&\
 popd
--- slurm_script.sh
+++ slurm_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --job-name=acl23doc2dial_1_epoch
-#SBATCH --output=/ukp-storage-1/tran/res.%j.%N.one.epoch.txt
+#SBATCH --job-name=acl23doc2dial_lang_token
+#SBATCH --output=/ukp-storage-1/tran/res.%j.%N.lang_token.txt
 #SBATCH --mail-user=an.tran@stud.tu-darmstadt.de
 #SBATCH --mail-type=ALL
 #SBATCH --account=ukp-student
@@ -13,5 +13,5 @@
 source /ukp-storage-1/tran/miniconda3/etc/profile.d/conda.sh
 conda activate acl23doc2dial
 module purge
-module load cuda/10.2
+module load cuda
 bash /ukp-storage-1/tran/DAMO-ConvAI/acl23doc2dial/run.sh
