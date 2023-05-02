c82fb1b203078acabbd42c35ed5e66678c3530e3
AnTrn95 <antr.hat@gmail.com>
2023-05-02 22:51:46 +0200

    fiixed bug: ignore empty df when adding language token

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -4,12 +4,8 @@ from modelscope.utils.logger import get_logger
 from sklearn.model_selection import train_test_split
 from collections import defaultdict
 import pandas as pd
+from torch import nn 
 from typing import Union
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style='whitegrid')
-sns.set_palette('pastel')
 
 logger = get_logger()
 
@@ -45,9 +41,23 @@ def test_split(dataset:pd.DataFrame, test_size:float=0.1):
     
     return ([],[]) if len(list(dataset)) == 0 else train_test_split(dataset, test_size=test_size)
 
-def add_lang_token(dataset:pd.DataFrame, lang_key:str, colnames:list=["query"], token_colname:str="lang"):
-    logger.info(f"adding special language token {lang_key} to input query...")
 
+def add_lang_token(dataset:Union[pd.DataFrame, list], lang_key:str, colnames:list=["query"], token_colname:str="lang"):
+    """add language token to query
+
+    Args:
+        dataset (pd.DataFrame): df containing queries
+        lang_key (str): specifies language token
+        colnames (list, optional): column names containing the queries that shall be appended by the lang token. Defaults to ["query"].
+        token_colname (str, optional): new column name where language token will be stored additionally. Defaults to "lang".
+
+    Returns:
+        _type_: _description_
+    """
+    logger.info(f"adding special language token {lang_key} to input query...")
+    if len(dataset) == 0:
+        return None 
+    
     def concat_special_token(col):
         try:
             is_list = not isinstance(eval(col.iloc[0]), str)
@@ -65,5 +75,34 @@ def add_lang_token(dataset:pd.DataFrame, lang_key:str, colnames:list=["query"], 
     return dataset
 
 
+def resize_token_embeddings(model, new_token_size):
+    """resize embedding layer of Modelscope models to fit updated number of tokens
 
+    Args:
+        model (modelscope.models.Model): model to resize embeddings
+        new_token_size (int): new token embedding size
 
+    Returns:
+        modelscope.models.Model: model with resized token embeddings
+    """
+    embedding_layer = model.embeddings.word_embeddings
+    old_num_tokens, old_embedding_dim = embedding_layer.weight.shape
+
+    # create new embedding layer with new token emb size
+    new_embeddings = nn.Embedding(
+            new_token_size, old_embedding_dim
+    )
+
+    new_embeddings.to(
+        embedding_layer.weight.device,
+        dtype=embedding_layer.weight.dtype,
+    )
+
+    # copying old entries
+    new_embeddings.weight.data[:old_num_tokens, :] = embedding_layer.weight.data[
+        :old_num_tokens, :
+    ]
+
+    model.embeddings.word_embeddings = new_embeddings
+
+    return model
