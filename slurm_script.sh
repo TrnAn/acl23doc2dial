ad4baca9cde60d98266906175d2e2cc34bfa1852
An Tran <tran@slurm-login.ukp.informatik.tu-darmstadt.de>
2023-05-09 02:59:35 +0200

    updated slurm_script.sh

--- slurm_script.sh
+++ slurm_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-#SBATCH --job-name=acl23doc2dial_lang_token
-#SBATCH --output=/ukp-storage-1/tran/res.%j.%N.lang_token.txt
+#SBATCH --job-name=acl23doc2dial_inference_lang_token
+#SBATCH --output=/ukp-storage-1/tran/res.%j.%N.inference_lang_token.txt
 #SBATCH --mail-user=an.tran@stud.tu-darmstadt.de
 #SBATCH --mail-type=ALL
 #SBATCH --account=ukp-student
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=64GB
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 
 nvidia-smi
 source /ukp-storage-1/tran/miniconda3/etc/profile.d/conda.sh
